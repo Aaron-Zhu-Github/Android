8ce875a795d168ea557bcfc55e70f3d71f7e2231
Junren(Aaron) Zhu <107200188+Aaron-Zhu-Github@users.noreply.github.com>
2022-09-13 22:26:42 -0400

    Add files via upload

---
+++ Android_Photos/app/src/main/java/com/picture/view/MyImageView.java
@@ -0,0 +1,28 @@
+package com.picture.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.appcompat.widget.AppCompatImageView;
+
+public class MyImageView extends AppCompatImageView {
+
+    public MyImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyImageView(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        setMeasuredDimension(getDefaultSize(0, widthMeasureSpec),
+                getDefaultSize(0, heightMeasureSpec));
+        int childWidthSize = getMeasuredWidth();
+
+        heightMeasureSpec = widthMeasureSpec = MeasureSpec.makeMeasureSpec(
+                childWidthSize, MeasureSpec.EXACTLY);
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+}
